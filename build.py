96ed6a1124fca0313d2e50444d40cbd8970db189
arj <arj.python@gmail.com>
2019-03-25 00:24:19 +0400

    init

---
+++ build.py
@@ -0,0 +1,59 @@
+from os import listdir
+import jinja2
+import markdown
+
+
+def build(mdfile):
+    '''
+    adapted from https://gist.github.com/jiffyclub/5015986
+    added highlight.js to support code
+    ```python
+    print(1)
+    ```
+    '''
+    TEMPLATE = """<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/atelier-forest-light.min.css">
+        <style>
+
+        </style>
+    </head>
+    <body>
+    <div class="container">
+
+    {{content}}
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    </div>
+    </body>
+    </html>
+    """
+
+    name = mdfile[:-3]
+    md_file = 'md/{}.md'.format(name)
+    html_file = 'docs/{}.html'.format(name)
+
+    with open(md_file, 'r', encoding='utf8') as f:
+        md = f.read()
+
+    extensions = ['extra', 'smarty']
+
+    html = markdown.markdown(md, extensions=extensions, output_format='html5')
+    doc = jinja2.Template(TEMPLATE).render(content=html)
+    with open(html_file, 'w+', encoding='utf8') as f:
+        f.write(doc)
+
+def get_all_mdfile():
+    return listdir('md/')
+
+def build_all():
+    for mdfile in get_all_mdfile():
+        build('{}'.format(mdfile))
+
+if __name__ == '__main__':
+    build_all()
